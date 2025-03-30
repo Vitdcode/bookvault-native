b031bbd1a49dc20ad81515ad20911de848de4f45
Vitdcode <vitalijdietrich@gmail.com>
2025-03-30 19:50:25 +0200

    feat: added a toucableRipple effect from the native paper ui library

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { TextInput, Text } from "react-native-paper";
+import { TextInput, Text, TouchableRipple } from "react-native-paper";
 import { useAppContext } from "../context/context";
 import handleFetch from "../functional functions_components/fetchBooks";
 import { View, ScrollView, Image, TouchableOpacity } from "react-native";
@@ -23,24 +23,29 @@ export default function SearchScreen() {
       />
       <ScrollView style={{ marginBottom: 150 }}>
         {fetchedBooks.map((book) => (
-          <View key={book.googleBooksId} style={{ padding: 20, flexDirection: "row", gap: 10 }}>
-            <Image
-              source={{ uri: book.coverUrl }}
-              resizeMethod="contain"
-              style={{
-                height: 200,
-                width: 130,
-                borderRadius: 5,
-                elevation: 3,
-              }}
-            />
-            <View style={{ padding: 5, width: "60%", gap: 10 }}>
-              <Text variant="titleLarge">{book.title}</Text>
-              <Text variant="bodySmall" style={{ color: "gray" }}>
-                {book.authors.join(", ")}
-              </Text>
+          <TouchableRipple
+            onPress={() => console.log("Pressed")}
+            rippleColor="rgba(214, 214, 214, 0.32)"
+          >
+            <View key={book.googleBooksId} style={{ padding: 20, flexDirection: "row", gap: 10 }}>
+              <Image
+                source={{ uri: book.coverUrl }}
+                resizeMethod="contain"
+                style={{
+                  height: 200,
+                  width: 130,
+                  borderRadius: 5,
+                  elevation: 3,
+                }}
+              />
+              <View style={{ padding: 5, width: "60%", gap: 10 }}>
+                <Text variant="titleLarge">{book.title}</Text>
+                <Text variant="bodySmall" style={{ color: "gray" }}>
+                  {book.authors.join(", ")}
+                </Text>
+              </View>
             </View>
-          </View>
+          </TouchableRipple>
         ))}
       </ScrollView>
     </View>
